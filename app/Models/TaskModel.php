38c53dd0a56278d5927773d2bed33de521458a8d
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-30 22:14:35 +0100

    function find all

--- app/Models/TaskModel.php
+++ app/Models/TaskModel.php
@@ -2,6 +2,8 @@
 namespace App\Models;
 
 require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/Task.php';
+
 
 class TaskModel {
     private $db;
@@ -10,5 +12,25 @@ class TaskModel {
         $database = \Database::getInstance();
         $this->db = $database->getConnection();
     }
+
+    public function findAll($filter = null) {
+        $sql = "SELECT * FROM tasks";
+        
+        if ($filter === 'done') {
+            $sql .= " WHERE done = 1";
+        } elseif ($filter === 'pending') {
+            $sql .= " WHERE done = 0";
+        }
+        
+        $sql .= " ORDER BY created_at DESC";
+        $stmt = $this->db->query($sql);
+        
+        $tasks = [];
+        while ($row = $stmt->fetch()) {
+            $tasks[] = new Task($row);
+        }
+        return $tasks;
+    }
+    
 }
 ?>
